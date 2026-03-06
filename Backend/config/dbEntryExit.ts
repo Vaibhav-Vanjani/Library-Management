a1a651fdbdcb35999ac442934c2b9d4644ed0e92
Vaibhav-Vanjani <vaibhavvanjani234@gmail.com>
2026-03-06 17:35:59 +0530

    driver adapter issue fix

--- Backend/config/dbEntryExit.ts
+++ Backend/config/dbEntryExit.ts
@@ -7,6 +7,18 @@ console.log("i am in dbEntryExit.ts");
 const PORT = process.env.ENTRY_EXIT_DB_PORT;
 const CONNECTION_LIMIT = process.env.ENTRY_EXIT_DB_CONNECTION_LIMIT;
 
+// const connection_obj = { host: process.env.ENTRY_EXIT_DB_HOST!,
+//   port: 21133,
+//   connectionLimit: 10,
+//   user:process.env.ENTRY_EXIT_DB_USER!,
+//   password:process.env.ENTRY_EXIT_DB_PASSWORD!,
+//   database:process.env.ENTRY_EXIT_DB_DATABASE!,
+//   allowPublicKeyRetrieval: true,
+//   ssl:true
+// }
+
+// console.log("connection_obj______",connection_obj);
+
 const adapter = new PrismaMariaDb({
   host: process.env.ENTRY_EXIT_DB_HOST!,
   port: 21133,
@@ -15,6 +27,7 @@ const adapter = new PrismaMariaDb({
   password:process.env.ENTRY_EXIT_DB_PASSWORD!,
   database:process.env.ENTRY_EXIT_DB_DATABASE!,
   allowPublicKeyRetrieval: true,
+  ssl:true
 })
 
 const globalForPrisma = global as unknown as { prismab: PrismaClient };
@@ -22,7 +35,7 @@ const globalForPrisma = global as unknown as { prismab: PrismaClient };
 export const entryExitDB =
   globalForPrisma.prismab || new PrismaClient({adapter});
 
-console.log(entryExitDB,"entryExitDBB");
+// console.log(entryExitDB,"entryExitDBB");
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prismab = entryExitDB;
