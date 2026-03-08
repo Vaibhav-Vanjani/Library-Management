50a5651c71e0eebc8280eee23314d044ba47b8e8
Vaibhav-Vanjani <vaibhavvanjani234@gmail.com>
2026-03-08 21:45:23 +0530

    added test log

--- Backend/utils/sendPushNotificationUtils.ts
+++ Backend/utils/sendPushNotificationUtils.ts
@@ -1,7 +1,11 @@
 const url = "https://exp.host/--/api/v2/push/send";
 
 export function sendExpoPushNotification({expoTokenList,title,description}:{expoTokenList:string[],title:string,description:string}){   
+    
+    console.log(expoTokenList,title,description,"____________in sendExpoPushNotification")
+    
     expoTokenList.forEach(async expoToken => {
+         console.log(expoToken,title,description,"____________in sendExpoPushNotification forEach")
         try {
                 await fetch(url, {
                     method: "POST",
